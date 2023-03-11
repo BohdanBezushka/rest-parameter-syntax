abab0873518f6ff5e746fc6c8e5e394dd7ae725b
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-03-11 16:14:24 +0000

    Finish the challenge

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -4,7 +4,36 @@
  */
 
 // Regular function call 
+const sumAll = (a, b, c) => a+ b + c;
+let sum = sumAll(1, 1 , 3);
+console.log(sum)
 
 // Extra arguments are ignored
+let sum2 = sumAll(1, 2 , 3, 4, 5, 6);
+console.log(sum2);
 
 // Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest){
+        sum += i;
+    }
+    return sum
+}
+
+let sum3 = sumRest(1,2,3,4,5,6);
+
+console.log(sum3)
+
+// Challenge:
+let mixedLetters = ['b', 'd', 'a', 'c', 'f', 'e'];
+
+let moreMixedLetters = [...mixedLetters,'h', 'k', 'g', 'j', 'i', 'l'];
+console.log(moreMixedLetters);
+const updateSortReverse = (arr, ...letters) => {
+   let sortedReverse = [...arr, ...letters].sort().reverse();
+   return sortedReverse;
+};
+let reverseSort = updateSortReverse(moreMixedLetters, 'n', 'm', 'o');
+console.log(reverseSort);
+console.log(mixedLetters);
